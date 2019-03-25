74998f94ebbcb6afbe1f0cbcde513647520457c5
Philipp <philippboensch@gmail.com>
2019-03-25 20:19:15 +0100

    comb logic finished

--- bee/scripts/Comb.gd
+++ bee/scripts/Comb.gd
@@ -1,17 +1,44 @@
 extends Node2D
 
 enum CombType { BEE_COMB, HONEY_COMB }
-
 var comb_type = CombType.BEE_COMB # CombType
+var evolution_states: int
+
+signal comb_evolution_finished
+
+func start_evolution() -> void:
+	$EvolutionTimer.start()
+	
+func stop_evolution() -> void:
+	$EvolutionTimer.stop()
+
+func set_evolution_time(new_val: float) -> void:
+    $EvolutionTimer.wait_time = new_val
+
+func set_evolution_state(new_val: int) -> void:
+	assert new_val < evolution_states
+	$AnimatedSprite.frame = new_val
+	if $AnimatedSprite.frame == evolution_states -1:
+		emit_signal("comb_evolution_finished")
+		stop_evolution()
+		print("evolution finished")
+
+func increment_evolution_state() -> void:
+	set_evolution_state($AnimatedSprite.frame + 1)
+
+func get_evolution_state() -> int:
+    return $AnimatedSprite.frame
 
 func _ready() -> void:
-	#$EvolutionTimer.start()
-	
 	match comb_type:
 		CombType.BEE_COMB:
 			$AnimatedSprite.animation = "bee"
 		CombType.HONEY_COMB:
 			$AnimatedSprite.animation = "honey"
+	
+	evolution_states = $AnimatedSprite.frames.get_frame_count($AnimatedSprite.animation)
+	start_evolution()
 
 func _on_EvolutionTimer_timeout() -> void:
-	$AnimatedSprite.frame = $AnimatedSprite.frame + 1
+	if $AnimatedSprite.frame < evolution_states:
+		increment_evolution_state()
