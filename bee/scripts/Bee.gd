de5a48448ba170a83305cf579ec2841da12e60e1
Bluemi <bruno.schilling@protonmail.ch>
2019-04-03 18:50:41 +0200

    bees go out of the hive to collect nectar now

--- bee/scripts/Bee.gd
+++ bee/scripts/Bee.gd
@@ -1,54 +1,20 @@
 extends Node2D
 
-enum BeeState { IDLE, COLLECT_NECTAR }
-
 const NECTAR_POINT = Vector2(550, 480)
 const COMB_POINT = Vector2(260, 1200)
-const BEE_SPEED = 4
+const NECTAR_COLLECT_TIMER = 160
 
 var current_task = null
 
-class Task:
-	func finished():
-		assert false
-	
-	func tick_bee(bee):
-		assert false
-
-class CollectNectarTask extends Task:
-	var has_nectar = false
-	var nectar_point
-	var comb_point
-	
-	func _init(n_point, c_point):
-		self.nectar_point = n_point
-		self.comb_point = c_point
-
-	func finished():
-		return false
-
-	func tick_bee(bee):
-		if has_nectar:
-			var to_comb = self.comb_point - bee.position
-			var diff = to_comb.length()
-			to_comb = to_comb.clamped(BEE_SPEED)
-			bee.position += to_comb
-			if diff < 1:
-				has_nectar = false
-		else:
-			var to_nectar = self.nectar_point - bee.position
-			var diff = to_nectar.length()
-			to_nectar = to_nectar.clamped(BEE_SPEED)
-			bee.position += to_nectar
-			if diff < 1:
-				has_nectar = true
-
-func _process(delta):
+func _process(_delta):
 	# execute first task
 	if current_task == null:
 		_do_idle()
 	else:
-		current_task.tick_bee(self)
+		if current_task.finished():
+			current_task = null
+		else:
+			current_task.tick_bee(self)
 
 func idle():
 	current_task = null
@@ -61,4 +27,38 @@ func _do_idle():
 	self.position.y += rand_range(-1, 1)
 
 func collect_nectar():
-	current_task = CollectNectarTask.new(NECTAR_POINT, COMB_POINT)
+	current_task = CollectNectarTask.new(NECTAR_POINT, COMB_POINT, NECTAR_COLLECT_TIMER)
+
+func fade_out():
+	$FadeTween.interpolate_property($Sprite,
+									"modulate",
+									Color(1, 1, 1, 1),
+									Color(1, 1, 1, 0),
+									1,
+									Tween.TRANS_LINEAR,
+									Tween.EASE_IN)
+	$FadeTween.interpolate_property(self,
+									"position",
+									self.position,
+									self.position + Vector2(0, -20),
+									1,
+									Tween.TRANS_LINEAR,
+									Tween.EASE_IN)
+	$FadeTween.start()
+
+func fade_in():
+	$FadeTween.interpolate_property($Sprite,
+									"modulate",
+									Color(1, 1, 1, 0),
+									Color(1, 1, 1, 1),
+									1,
+									Tween.TRANS_LINEAR,
+									Tween.EASE_IN)
+	$FadeTween.interpolate_property(self,
+									"position",
+									self.position,
+									self.position + Vector2(0, 20),
+									1,
+									Tween.TRANS_LINEAR,
+									Tween.EASE_IN)
+	$FadeTween.start()
---
+++ bee/scripts/tasks/CollectNectar.gd
@@ -0,0 +1,44 @@
+extends Task
+
+class_name CollectNectarTask
+
+const BEE_SPEED = 3
+
+enum BeeState { IN_HAS_NECTAR, IN_NO_NECTAR, OUT }
+
+var nectar_point
+var comb_point
+var bee_state = BeeState.IN_NO_NECTAR
+var out_counter
+var out_counter_max
+	
+func _init(n_point, c_point, ocm):
+	self.nectar_point = n_point
+	self.comb_point = c_point
+	self.out_counter_max = ocm
+
+func finished():
+	return false
+
+func tick_bee(bee):
+	if bee_state == BeeState.IN_HAS_NECTAR:
+		var to_comb = self.comb_point - bee.position
+		var diff = to_comb.length()
+		to_comb = to_comb.clamped(BEE_SPEED)
+		bee.position += to_comb
+		if diff < 1:
+			bee_state = BeeState.IN_NO_NECTAR
+	elif bee_state == BeeState.IN_NO_NECTAR:
+		var to_nectar = self.nectar_point - bee.position
+		var diff = to_nectar.length()
+		to_nectar = to_nectar.clamped(BEE_SPEED)
+		bee.position += to_nectar
+		if diff < 1:
+			bee_state = BeeState.OUT
+			out_counter = out_counter_max + rand_range(-50, 50)
+			bee.fade_out()
+	elif bee_state == BeeState.OUT:
+		out_counter -= 1
+		if out_counter < 1:
+			bee_state = BeeState.IN_HAS_NECTAR
+			bee.fade_in()
